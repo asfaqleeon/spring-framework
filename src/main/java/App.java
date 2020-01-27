996da31e68923dacf54ef138c54690e9887d2e8a
asfaqleeon <asfaqleeon@gmail.com>
2020-01-27 22:20:13 +0600

    first day

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import beans.Tyre;
+import beans.Vehicle;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+
+        //Vehicle obj = (Vehicle) applicationContext.getBean("bike");
+        //obj.drive();
+
+        Tyre tyre = (Tyre) applicationContext.getBean("tyre");
+        System.out.println(tyre);
+    }
+}
---
+++ src/main/java/beans/Vehicle.java
@@ -0,0 +1,5 @@
+package beans;
+
+public interface Vehicle {
+    void drive();
+}
